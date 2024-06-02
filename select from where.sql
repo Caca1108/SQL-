39e73bb428969da887f7732512608b9e7c524d59
Caca DS <stevi0811@gmail.com>
2024-06-02 09:09:52 +0700

    tahap dasar
    materi dasar sql jadi bisa dipelajari kembali jika lupa

---
+++ select from where.sql
@@ -0,0 +1,9 @@
+select * 
+from actor;
+
+select first_name , last_name 
+from actor a;
+
+select first_name , last_name 
+from actor a
+where first_name = 'Nick'; #### btw disini ga bisa petik dua ("")
